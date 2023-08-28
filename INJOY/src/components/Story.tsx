e91b9fdb88a8d18e096529ecd2a56a274d994774
Mammadli17 <isamammadli17@gmail.com>
2023-08-28 20:21:11 +0400

    c

--- INJOY/src/components/Story.tsx
+++ INJOY/src/components/Story.tsx
@@ -1,6 +1,6 @@
-import { View, Text, Dimensions, Image, TouchableOpacity, Modal, Pressable, StyleSheet } from 'react-native'
+import { View, Text, Dimensions, Image, TouchableOpacity, Modal, Pressable, StyleSheet, Animated, FlatList } from 'react-native'
 import React, { useState, useEffect, useMemo } from 'react'
-import { useDispatch, useSelector,shallowEqual } from 'react-redux';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import { AppDispatch } from '../redux';
 import { useFocusEffect } from '@react-navigation/native';
 import { fetchUsers } from '../redux/slices/User';
@@ -20,53 +20,96 @@ const Story = ({ navigation }: any) => {
     const [user, setuser] = useState<any>()
     const [ModalVisible, setModalVisible] = useState<any>(false)
     const [ModalVisible1, setModalVisible1] = useState<any>(false)
+    const [ModalVisible2, setModalVisible2] = useState<any>(false)
     const data = useSelector((state: any) => state.AllUsers.data, shallowEqual);
+    const [story, setstory] = useState<any>()
+    const dispatch: AppDispatch = useDispatch();
+    const fillAnimation = new Animated.Value(0);
 
-  const dispatch: AppDispatch = useDispatch();
+    const [dataFetched, setDataFetched] = useState(false);
+    const slideInAnimation = new Animated.Value(-screenWidth);
+    const slideIn = () => {
+        Animated.timing(slideInAnimation, {
+            toValue: 0,
+            duration: 300,
+            useNativeDriver: false,
+        }).start();
+    };
 
-  const [dataFetched, setDataFetched] = useState(false); 
+    // Function to handle slide-out animation
+    const slideOut = () => {
+        Animated.timing(slideInAnimation, {
+            toValue: -screenWidth,
+            duration: 300,
+            useNativeDriver: false,
+        }).start(() => {
+            setModalVisible2(false); // Close the modal after animation
+        });
+    };
 
-  useEffect(() => {
-    dispatch(fetchUsers())
-      .then(() => {
-        setDataFetched(true); 
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-    
-    fetchUserData();
-  }, [dispatch]);
+    // Show the modal and start the animation when needed
+    useEffect(() => {
+        if (ModalVisible2) {
+            slideIn();
+            Animated.timing(fillAnimation, {
+                toValue: 1,
+                duration: 4000, // Same duration as the modal auto-close
+                useNativeDriver: false,
+            }).start(() => {
+                slideOut();
+            });
+        } else {
+            fillAnimation.setValue(0); // Reset the progress when the modal is closed
+        }
+    }, [ModalVisible2]);
 
-  useEffect(() => {
-    if (dataFetched && data.length > 0) {
-      const filteredFollower = data.find((itemm: any) => itemm._id === userr._id);
-      setuser(filteredFollower);
-    }
-  }, [data, dataFetched, userr]);
-  useEffect(() => {
-    dispatch(fetchUsers());
-    fetchUserData();
-  }, [dispatch,]);
+    useEffect(() => {
+        dispatch(fetchUsers())
+            .then(() => {
+                setDataFetched(true);
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
 
-  const fetchUserData = async () => {
-    const userData: any = await AsyncStorage.getItem('user');
-    const userr = JSON.parse(userData);
-    setuserr(userr);
+        fetchUserData();
+    }, [dispatch]);
 
-    const filteredFollower = data.find((itemm: any) => itemm._id === userr._id);
-    console.log(filteredFollower);
-    
-    setuser(filteredFollower);
-  };
+    useEffect(() => {
+        if (dataFetched && data.length > 0) {
+            const filteredFollower = data.find((itemm: any) => itemm._id === userr._id);
+            setuser(filteredFollower);
+            const store = data.filter((itemm: any) => itemm._id != userr._id);
+            setstory(store)
 
-  useFocusEffect(
-    React.useCallback(() => {
-      dispatch(fetchUsers());
-      fetchUserData();
-    }, [dispatch])
-  );
-            
+            setuser(filteredFollower);
+        }
+    }, [data, dataFetched, userr]);
+    useEffect(() => {
+        dispatch(fetchUsers());
+        fetchUserData();
+    }, [dispatch,]);
+
+    const fetchUserData = async () => {
+        const userData: any = await AsyncStorage.getItem('user');
+        const userr = JSON.parse(userData);
+        setuserr(userr);
+
+        const filteredFollower = data.find((itemm: any) => itemm._id === userr._id);
+        setuser(filteredFollower);
+        const store = data.filter((itemm: any) => itemm._id != userr._id);
+        setstory(store)
+    };
+
+    useFocusEffect(
+        React.useCallback(() => {
+            dispatch(fetchUsers());
+            fetchUserData();
+        }, [dispatch])
+    );
+
+    console.log(story);
+
     const handleImageSelect = () => {
         launchImageLibrary(
             {
@@ -81,64 +124,87 @@ const Story = ({ navigation }: any) => {
             }
         );
     };
-    console.log(user,"us");
-    
+    const Func = () => {
+        setModalVisible2(true)
+        setModalVisible(false)
+    }
+
+    const render = ({ item }: any) => {
+        return (
+
+
+            <View style={{marginLeft:20,alignItems:"center"}}>
+                <Image
+                    source={user && user && user.profilepicture ? { uri: item.profilepicture } : require('../assets/pictures/profile.jpg')}
+                    style={{ height: 70, width: 70, borderRadius: 100 }}
+                    resizeMode="cover"
+                />
+                <Text style={{ color: "white", fontSize: 12, alignItems: "center", top: 3 }}>
+                    {item.FullName}
+                </Text>
+            </View>
+        )
+    }
     const handleImageUpload = () => {
 
         if (!newImage || !newImage.assets || newImage.assets.length === 0) {
-          return;
-        }  
-       console.log(newImage);
-       
-          
+            return;
+        }
+
         const formData = new FormData();
         formData.append('story', {
-          uri: newImage.assets[0].uri,
-          name: newImage.assets[0].fileName,
-          type: newImage.assets[0].type,
+            uri: newImage.assets[0].uri,
+            name: newImage.assets[0].fileName,
+            type: newImage.assets[0].type,
         });
         formData.append('userId', userr._id);
-    
         axios
-          .post('http://192.168.100.31:8080/api/story', formData, {
-            headers: {
-              'Content-Type': 'multipart/form-data',
-            },
-          })
-          .then(() => {
-            console.log('Upload successful!');
-          })
-          .catch((error) => {
-            console.log("ssssssssssssssssssssssss");
-            
-          })
-          .finally(() => {
-    
-         
-          });
- 
-      };
+            .post('http://192.168.100.31:8080/api/story', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            })
+            .then(() => {
+                setModalVisible1(false)
+                dispatch(fetchUsers())
+            })
+            .catch((error) => {
+
+            })
+            .finally(() => {
+
+
+            });
+
+    };
 
 
     return (
         <View style={{ height: screenHeight * 0.14, top: 10, flexDirection: "row" }}>
-            <TouchableOpacity onPress={() => setModalVisible(true)}>
-                <View style={{ top: 5, left: 10 }}>
 
-                    <Image
-                        source={user && user && user.profilepicture ? { uri: user.profilepicture } : require('../assets/pictures/profile.jpg')}
-                        style={{ height: 70, width: 70, borderRadius: 100 }}
-                        resizeMode="cover"
-                    />
-                    <View style={{ position: "absolute", top: 50, left: 50 }}>
-                        <Storyy />
-                    </View>
-                    <Text style={{ color: "white", fontSize: 12, alignItems: "center", left: 5, top: 3 }}>
-                        Your Story
-                    </Text>
-
+            <View style={{ top: 5, left: 10, flexDirection: "row" }}>
+                <View>
+                    <TouchableOpacity onPress={() => setModalVisible(true)}>
+                        <Image
+                            source={user && user && user.profilepicture ? { uri: user.profilepicture } : require('../assets/pictures/profile.jpg')}
+                            style={{ height: 70, width: 70, borderRadius: 100 }}
+                            resizeMode="cover"
+                        />
+                        <View style={{ position: "absolute", top: 50, left: 50 }}>
+                            <Storyy />
+                        </View>
+                        <Text style={{ color: "white", fontSize: 12, alignItems: "center", left: 5, top: 3 }}>
+                            Your Story
+                        </Text>
+                    </TouchableOpacity>
                 </View>
-            </TouchableOpacity>
+                <FlatList
+                    data={story}
+                    renderItem={render}
+                    horizontal
+                />
+            </View>
+
             <Modal
                 animationType="slide"
                 transparent={true}
@@ -158,9 +224,11 @@ const Story = ({ navigation }: any) => {
                                     Add your story
                                 </Text>
                             </TouchableOpacity>
-                            <Text style={{ color: "white", fontSize: 20 }}>
-                                View your story
-                            </Text>
+                            <TouchableOpacity onPress={Func}>
+                                <Text style={{ color: "white", fontSize: 20 }}>
+                                    View your story
+                                </Text>
+                            </TouchableOpacity>
                         </View>
                     </View>
                 </View>
@@ -171,7 +239,7 @@ const Story = ({ navigation }: any) => {
                 transparent={true}
                 onRequestClose={() => setModalVisible1(false)}
             >
-    
+
                 <View style={styles.modalContainer}>
                     {newImage && (
                         <Image
@@ -185,18 +253,51 @@ const Story = ({ navigation }: any) => {
                             <Pressable style={{ right: '100%', marginTop: '1%' }} onPress={() => setModalVisible1(false)}>
                                 <Cancel />
                             </Pressable>
-                            <Pressable  onPress={handleImageUpload}>
+                            <Pressable onPress={handleImageUpload}>
                                 <Done />
                             </Pressable>
                         </View>
                     </View>
                 </View>
             </Modal>
+            <Modal
+                visible={ModalVisible2}
+                onRequestClose={() => setModalVisible2(false)}
+            >
+                <View style={styles.modalContainer}>
+                    {
+                        user?.story ?
+                        <Image
+                        source={{ uri: user?.story }}
+                        style={{ width: screenWidth, height: screenHeight }}
+                        resizeMode="cover"
+                    />
+                    :
+                    <View>
+                        <Text style={{color:"gray",fontSize:40}}>
+                            No Status, Pls add your status
+                        </Text>
+                        </View>
+                    }
+                    <View style={styles.progressContainer}>
+                        <Animated.View
+                            style={[
+                                styles.progressBar,
+                                {
+                                    width: fillAnimation.interpolate({
+                                        inputRange: [0, 1],
+                                        outputRange: ['0%', '100%'],
+                                    })
+                                }
+                            ]}
+                        />
+                    </View>
+                </View>
+            </Modal>
+
         </View>
     )
 }
-
-
 export default Story
 const styles = StyleSheet.create({
     container: {
@@ -228,14 +329,27 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.7)',
+        backgroundColor:"#131621"
+
     },
     selectedImage: {
-        width: screenWidth / 2,
-        height: screenWidth / 2,
-        borderRadius: 300,
+        width: screenWidth / 1.5,
+        height: screenWidth / 1.2,
+        borderRadius: 20,
         marginBottom: 20,
+    }, progressContainer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        width: '100%',
+        height: 5,
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',
     },
+    progressBar: {
+        height: '100%',
+        backgroundColor: 'white',
+    },
+
     modalButtons: {
         flexDirection: 'row',
         justifyContent: 'space-around',
